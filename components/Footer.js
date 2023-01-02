28f902e87630af5f7b7f1f2588492b031ab4f3a0
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-02 18:13:46 +0100

    Forgot to add the component file.

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import { Box, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <Box as="footer" mt={6}>
+      <Text
+        backgroundColor={'var(--background)'}
+        color={'var(--text-primary)'}
+        p={2}
+        fontSize={'sm'}
+      >
+        © 2022 - Made with ⌨️ by
+        <Link href={'https://github.com/leo-num/skrivbok'}> leo-num</Link>
+      </Text>
+    </Box>
+  )
+}

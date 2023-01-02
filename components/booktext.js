bac79593cd9b0e8141d2860f3a36d4c6427b43f7
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-02 20:53:14 +0100

    Added a simple timer.

--- components/booktext.js
+++ components/booktext.js
@@ -4,8 +4,9 @@ import { Heading, Text } from '@chakra-ui/react'
 
 export default function BookText() {
   const [position, setPosition] = useState(0)
-
   const [thumbsUpVisible, setThumbsUpVisible] = useState(false)
+  const [timeElapsed, setTimeElapsed] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
 
   const paragraph =
     'Den bok jag nu sätter mig ner att skriva måste verka meningslös på många - om jag alls vågar tänka mig, att "många" får läsa den - eftersom jag alldeles självmant, utan någons order, börjar ett sådant arbete och ändå inte själv är riktigt på det klara med vad avsikten är. Jag vill och måste, det är alltsammans. Allt mer och mer obönhörligt frågar man efter avsikten och planmässigheten i vad som göres och säges, så att helst inte ett ord ska falla på måfå - det är bara författaren till den här boken som har tvingats gå motsatta vägen, ut i det ändamålslösa. Ty fast mina år här som fånge och kemist - de måste vara över tjugu, tänker jag mig - har varit fulla nog ändå av arbete och brådska, måste det finnas något som inte tycker det är tillräckligt och som har lett och överblickat ett annat arbete inom mig, ett som jag själv inte hade någon möjlighet att överblicka och där jag ändå har varit djupt och nästan plågsamt medintresserad. Det arbetet kommer att vara slutfört, när jag väl har skrivit ner min bok. Jag inser alltså, hur förnuftsvidriga mina skriverier måste te sig inför allt rationellt och praktiskt tänkande, men jag skriver ändå.'
@@ -57,8 +58,29 @@ export default function BookText() {
     }
   }, [])
 
+  useEffect(() => {
+    if (position === 1) {
+      setIsRunning(true)
+    }
+
+    if (position === paragraph.length) {
+      setIsRunning(false)
+    }
+  }, [position])
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setTimeElapsed(prevTime => prevTime + 1)
+      }, 1000) // 1000 milliseconds = 1 second
+
+      return () => clearInterval(interval)
+    }
+  }, [isRunning])
+
   return (
     <>
+      <Text>Tid: {timeElapsed} sekunder</Text>
       <Heading fontSize="1.8rem">Kallocain av Karin Boye</Heading>
       <Heading mb={3} fontSize="1.5rem">
         Första kapitlet

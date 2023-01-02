8e2ec3104b9586695c726a7f12c0523f0ac7777d
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-02 18:18:36 +0100

    Issues importing Footer component during deployment. Works locally...

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Link from 'next/link'
-import Text from '@chakra-ui/react'
-
 import Layout from '../components/layout'
 import BookText from '../components/booktext'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (

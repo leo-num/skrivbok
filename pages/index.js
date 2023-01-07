ab6df53f9d3f519c57ca3bbe3b7b94b276d0844e
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-07 12:38:28 +0100

    Trying a different Footer import style.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // My components:
 import Layout from '../components/layout'
 import BookText from '../components/booktext'
-import Footer from '../components/footer'
+import Footer from '@components/footer'
 
 export default function Home() {
 	return (

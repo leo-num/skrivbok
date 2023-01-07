d3fb3a2bd1db968729446fd675aa9f05fb8660b7
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-07 12:30:02 +0100

    Deployment errors. Story of my day.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // My components:
+import Footer from '../components/footer'
 import Layout from '../components/layout'
 import BookText from '../components/booktext'
-import Footer from '../components/footer'
 
 export default function Home() {
 	return (

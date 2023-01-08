973e51943721fb69b3358f7bf316d1bdecfaebea
leo-num <43721954+leo-num@users.noreply.github.com>
2023-01-08 18:58:03 +0100

    Added some padding to the footer.

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ import { Box, Text } from '@chakra-ui/react'
 
 export default function Footer() {
 	return (
-		<Box as="footer" mt={6}>
+		<Box as="footer" mt={6} mb={6}>
 			<Text backgroundColor={'var(--background)'} color={'var(--text-primary)'} p={0} fontSize={'sm'}>
 				© {new Date().getFullYear()} - Made with ❤️ by
 				<Link href={'https://github.com/leo-num/skrivbok'}> leo-num</Link>
